d7f68fc335320ba467605ad25dbb7ca6bf2eb25e
Ivan Skiridomov <Ivan.Skiridomov@gmail.com>
2015-01-13 21:22:21 +0300

    prepare form

--- assets/zakaz.php
+++ assets/zakaz.php
@@ -1,32 +1,39 @@
 <?php
-require 'PHPMailerAutoload.php';
 
-$mail = new PHPMailer;
+if(isset($_POST['Submit']) {
+    require 'PHPMailerAutoload.php';
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.inbox.ru';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'diplom24@inbox.ru';                 // SMTP username
-$mail->Password = 'xxxXXX123';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+    $mail = new PHPMailer;
 
-$mail->From = 'diplom24@inbox.ru';
-$mail->FromName = 'Mailer';
-$mail->addAddress('bula_@mail.ru', 'Bula');     // Add a recipient
+    //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.inbox.ru';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'diplom24@inbox.ru';                 // SMTP username
+    $mail->Password = 'xxxXXX123';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->From = 'diplom24@inbox.ru';
+    $mail->FromName = 'Mailer';
+    $mail->addAddress('bula_@mail.ru', 'Bula');     // Add a recipient
 
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    $mail->isHTML(true);                                  // Set email format to HTML
+
+    $mail->Subject = 'Заказ с сайта';
+    $mail->Body    = 'Имя:'.$name.'<br>Email:'.$email.'<br> Сообщение: '.$message.' <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    if(!$mail->send()) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message has been sent';
+    }
 }
